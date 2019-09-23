61be9053d114ba74177eca3db127187fa7191477
Wisketchy Dobrov <warfolomey@gmail.com>
2019-09-23 22:49:00 +0300

    ssl error taihen

--- megamillpicker.py
+++ megamillpicker.py
@@ -3,6 +3,11 @@ import time
 import statistics
 import random
 
+import os, ssl
+if (not os.environ.get('PYTHONHTTPSVERIFY', '') and
+    getattr(ssl, '_create_unverified_context', None)): 
+    ssl._create_default_https_context = ssl._create_unverified_context
+
 print("Receiving quantum randomness...")
 time1 = time.time()
 quarray = []

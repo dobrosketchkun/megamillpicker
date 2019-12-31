93883c26d8aee33a295e4e2a5768b830e6f4f371
Wisketchy Dobrov <warfolomey@gmail.com>
2020-01-01 01:03:22 +0300

    fast

---
+++ fast_picker.py
@@ -0,0 +1,56 @@
+from http.client import HTTPSConnection                                        
+from json import dumps, loads                                                  
+
+#Оригинал - https://teletype.in/@snakeblog/rJnA9jwEX , модификации - мои
+
+API_TOKEN = ''  # Ваш токен                                                  
+                                                                               
+"""                                                                            
+Больше о формате запроса                                                        
+https://api.random.org/json-rpc/1/introduction                                  
+"""
+
+def random_int(min, max, n=2):
+    request_data = {  # Тело запроса                                                
+      'jsonrpc': '2.0',                                                            
+      'method': 'generateIntegers',                                                
+      'params': {                                                                  
+        'apiKey': API_TOKEN,                                                        
+        'min': min,  # Нижняя граница рандома                                        
+        'max': max,  # Верхняя граница                                              
+        'n': n,  # Количество запрашиваемых чисел                                  
+      },                                                                            
+      'id': 1,                                                                      
+    }                                                                              
+    encoded_data = dumps(request_data)                                              
+                                                                                   
+    headers = {                                                                    
+      'Content-Type': 'application/json-rpc',  # Тип запроса                        
+    }                                                                              
+    encoded_headers = dumps(headers)                                                
+                                                                                   
+    connection = HTTPSConnection('api.random.org')                                  
+    connection.request('GET', '/json-rpc/1/invoke', encoded_data, headers)          
+    response = connection.getresponse()                                            
+    response_data = loads(response.read().decode())                                
+    return response_data['result']['random']['data']
+
+def line_maker(lottery):
+    
+    if lottery == 'PowerBall':
+        big = 69
+        small = 26
+    elif lottery == 'Mega':
+        big = 70
+        small = 25
+    else:
+        print('Only PowerBall or Mega are supported!')
+    
+    a,b,c,d,e = random_int(1, big, n=5) # Доделать чтобы не выпадали одинаковые
+    f = random_int(1, small, n=1)[0]
+    print(f"\nYour numbers are {a}, {b}, {c}, {d}, {e} and {f}")
+
+
+for _ in range(2):
+    print('#' + str(_), end='')
+    line_maker('PowerBall')
